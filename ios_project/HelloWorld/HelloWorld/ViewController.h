1f7eb1509c91a28ea3fa2b89e7ac9cb59532579d
Matt Morgis <m.morgis@elsevier.com>
2017-01-24 14:59:01 -0500

    Creating a hello world sample iOS app

---
+++ ios_project/HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Morgis, Matthew (ELS-PHI) on 1/24/17.
+//  Copyright © 2017 Matt Morgis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
